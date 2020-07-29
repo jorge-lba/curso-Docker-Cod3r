8a636693339107fd33f13dcbb8555c43467b5a71
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-07-29 20:33:15 -0300

    Change - Variavel ambiente

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -15,8 +15,8 @@ class Sender(Bottle):
 
         db_host = os.getenv('DB_HOST', 'db')
         db_user = os.getenv('DB_USER', 'postgres')
-        db_name = os.getenv('DB_NAME', 'email_sender')
-        db_password = os.getenv('DB_PASSWORD', '"1234"')
+        db_name = os.getenv('DB_NAME', 'sender')
+        db_password = os.getenv('DB_PASSWORD', 'password')
 
         dsn = f'dbname={db_name} user={db_user} host={db_host} password={db_password}'
         self.conn = psycopg2.connect(dsn)
